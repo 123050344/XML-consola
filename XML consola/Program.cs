3b75b90f10637524708fc73519663337d7eef92a
Usuario <Usuario@DESKTOP-FT7LAG6>
2024-04-30 20:17:23 -0600

    Agregar archivos de proyecto.

---
+++ XML consola/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Xml;
+
+namespace appReadXML_From_URL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String URLString;
+            XmlReader reader;
+            URLString = "https://www.w3schools.com/xml/cd_catalog.xml";
+            reader = XmlReader.Create(URLString);
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        Console.Write("<" + reader.Name);
+                        while (reader.MoveToNextAttribute()) 
+                            Console.Write(" " + reader.Name + "='" + reader.Value + "'");
+                        Console.WriteLine(">");
+                        break;
+                    case XmlNodeType.Text: 
+                        Console.WriteLine(reader.Value);
+                        break;
+                    case XmlNodeType.EndElement: 
+                        Console.WriteLine("</" + reader.Name + ">");
+                        break;
+                }
+            }
+        }
+    }
+}
